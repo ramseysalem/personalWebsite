0ff346e215654c11a0b2073cf6c60c0c20ef8a41
tramseysalem <ramseysalem2@gmail.com>
2025-02-03 16:04:01 -0500

    started website

---
+++ react-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react"; 
+import {getImageUrl} from "../../src/utils";
+
+import styles from "./Navbar.module.css"
+
+export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    menuOpen; 
+    return (
+    <nav className={styles.navbar}>
+        <a className={styles.title} href = "/">
+            Portfolio
+            </a>
+        <div className={styles.menu}>
+            <img className={styles.menuBtn} 
+            src={
+                menuOpen 
+                ? getImageUrl("closeIcon.png")
+                : getImageUrl("menuIcon.png")
+            } 
+            alt="menu-button"
+            onClick={() => setMenuOpen(!menuOpen)}
+            />
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}>
+                <li>
+                    <a href = "#about">About</a>
+                </li>
+                <li>
+                    <a href = "#experience">Experience</a>
+                </li>
+                <li>
+                    <a href = "#projects">Projects</a>
+                </li>
+                <li>
+                    <a href = "#Contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>); 
+};
